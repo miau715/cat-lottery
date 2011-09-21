cf3ecf7714553b68d9c4d2d7b54ac710f741a9d4
Miau Huang <miau715@gmail.com>
2011-09-22 01:52:11 +0800

    create

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CatLottery::Application.config.secret_token = 'a2d42e75114429eccdd68298e15ead65d3d92950aa55ba37c2ffbef05b711d8fea7a362b467abeaa8c3b97221e594084e1c97da9e378f282f716848e545b167d'
