21216859f1e1df5a4aae01ed607bc0dc186385e5
Miau Huang <miau715@gmail.com>
2011-09-23 21:31:04 +0800

    add index and new

--- app/controllers/lottery_sources_controller.rb
+++ app/controllers/lottery_sources_controller.rb
@@ -1,2 +1,17 @@
 class LotterySourcesController < ApplicationController
+  def index
+    @lottery_sources = LotterySource.all
+  end
+  
+  def new
+    @lottery_sources = LotterySource.new
+  end
+  
+  def create
+    @lottery_sources = LotterySource.new(params[:lottery_source])
+    @lottery_sources.save
+  
+    redirect_to :action => :index
+  end
+  
 end
