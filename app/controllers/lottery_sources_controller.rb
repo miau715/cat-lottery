bcfc55969a80fa1665b9d196ae4c6df066407eab
Miau Huang <miau715@gmail.com>
2011-09-23 22:32:06 +0800

    end first step!

--- app/controllers/lottery_sources_controller.rb
+++ app/controllers/lottery_sources_controller.rb
@@ -11,7 +11,33 @@ class LotterySourcesController < ApplicationController
     @lottery_sources = LotterySource.new(params[:lottery_source])
     @lottery_sources.save
   
-    redirect_to :action => :index
+    redirect_to action: :index
   end
   
+  def show
+    @lottery_sources = LotterySource.find(params[:id])
+  end
+  
+  def edit
+    @lottery_sources = LotterySource.find(params[:id])
+  end
+  
+  def update
+    @lottery_sources = LotterySource.find(params[:id])
+    @lottery_sources.update_attributes(params[:lottery_source])
+  
+    redirect_to action: :show, id: @lottery_sources
+  end
+  
+  def destroy
+    @lottery_sources = LotterySource.find(params[:id])
+    @lottery_sources.destroy
+  
+    redirect_to action: :index
+  end
+  
+  def lottery
+    list_count = LotterySource.count
+    @lottery_sources = LotterySource.find(:first, offset: rand(list_count))
+  end
 end
