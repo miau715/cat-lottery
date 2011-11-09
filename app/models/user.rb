1717087edb3ffce2b01aa5163f85cfaf00a97846
Miau Huang <miau715@gmail.com>
2011-11-09 21:34:48 +0800

    all broken now!

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,8 @@ class User < ActiveRecord::Base
 
   # Setup accessible (or protected) attributes for your model
   attr_accessible :email, :password, :password_confirmation, :remember_me
+  
+  def is_admin?
+    is_admin
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -60,7 +60,7 @@ CatLottery::Application.routes.draw do
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
   # root :to => 'welcome#index'
-  #root :to => 'cats#index'
+  root :to => 'welcome#index'
 
   # See how all your routes lay out with "rake routes"
 
